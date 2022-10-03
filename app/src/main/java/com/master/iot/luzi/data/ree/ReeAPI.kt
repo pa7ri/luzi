9375726aff8297f76513751c9323aa411d9c4f90
Patricia Barrios <patricia.barrios.palacios@sap.com>
2022-10-03 23:17:55 +0200

    init code documentation

--- app/src/main/java/com/master/iot/luzi/data/ree/ReeAPI.kt
+++ app/src/main/java/com/master/iot/luzi/data/ree/ReeAPI.kt
@@ -4,7 +4,14 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+/**
+ * public API for Red Electrica de Espana: https://www.ree.es/en/apidatos
+ * Query basic format
+ * GET /{lang}/datos/{category}/{widget}?[query]
+ **/
 interface ReeAPI {
     @GET("/posts/{id}")
     fun getPostWithID(@Path("id") id: Int): Call<Post>
+
+
 }
